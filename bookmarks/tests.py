8d47f2643fa0e70a393aa066afab2733a80376b5
Francesco Banconi <frankban@gmail.com>
2011-07-31 18:26:07 +0200

    Added backend tests.

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -1,3 +1,126 @@
-__test__ = {"doctest": """
+import unittest
 
-"""}
+from django.db import models
+from django.contrib.auth.models import User
+
+from bookmarks import exceptions, backends
+
+
+class BookmarkTestModel(models.Model):
+    name = models.CharField(max_length=8)
+    
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class BookmarkTestMixin(object):
+    """
+    Mixin for tests.
+    """
+    def create_user(self, username):
+        return User.objects.create(username=username)
+    
+    def create_instance(self, name):
+        return BookmarkTestModel.objects.create(name=name)
+
+    def get_user_instance_key(self, prefix=''):
+        user = self.create_user('%suser' % prefix)
+        instance = self.create_instance('%sinstance' % prefix)
+        key = '%skey' % prefix
+        return user, instance, key
+
+    def check_bookmark(self, bookmark, user, instance, key):
+        self.assertEqual(bookmark.user, user)
+        self.assertEqual(bookmark.content_object, instance)
+        self.assertEqual(bookmark.key, key)
+    
+
+class BaseBackendTest(BookmarkTestMixin):
+    def test_add_bookmark(self):
+        user, instance, key = self.get_user_instance_key('add')
+        bookmark = self.backend.add(user, instance, key)
+        self.check_bookmark(bookmark, user, instance, key)
+
+    def test_add_same_bookmark(self):
+        user, instance, key = self.get_user_instance_key('addsame')
+        bookmark = self.backend.add(user, instance, key)
+        self.assertRaises(exceptions.AlreadyExists, 
+            self.backend.add, user, instance, key)
+
+    def test_bookmark_exists(self):
+        user, instance, key = self.get_user_instance_key('exists')
+        self.assertFalse(self.backend.exists(user, instance, key))
+        self.backend.add(user, instance, key)
+        self.assertTrue(self.backend.exists(user, instance, key))
+
+    def test_remove_bookmark(self):
+        user, instance, key = self.get_user_instance_key('remove')
+        self.backend.add(user, instance, key)
+        self.backend.remove(user, instance, key)
+        self.assertFalse(self.backend.exists(user, instance, key))
+
+    def test_remove_missing_bookmark(self):
+        user, instance, key = self.get_user_instance_key('missing')
+        self.assertRaises(exceptions.DoesNotExist, 
+            self.backend.remove, user, instance, key)
+
+    def test_filter_bookmarks(self):
+        user1 = self.create_user('user_filter_1')
+        user2 = self.create_user('user_filter_2')
+        instance1 = self.create_instance('instance_filter_1')
+        instance2 = self.create_instance('instance_filter_2')
+        key1, key2 = 'key_filter_1', 'key_filter_2'
+
+        bookmark1 = self.backend.add(user1, instance1, key1)
+        bookmark2 = self.backend.add(user2, instance1, key1)
+        bookmark3 = self.backend.add(user1, instance2, key1)
+        bookmark4 = self.backend.add(user1, instance2, key2)
+        bookmark5 = self.backend.add(user2, instance1, key2)
+
+        bookmarks_user1 = list(self.backend.filter_by(user1))
+        bookmarks_user2_key2 = list(self.backend.filter_by(user2, key=key2))
+        bookmarks_instance1 = list(self.backend.filter_for(instance1))
+        bookmarks_instance2_user1 = list(self.backend.filter_for(instance2, 
+            user=user1))
+        bookmarks_instance1_user2_key1 = list(self.backend.filter_for(instance1, 
+            user=user2, key=key1))
+        bookmarks_instance1_user1_key2 = list(self.backend.filter_for(instance1,
+            user=user1, key=key2))
+        
+        self.assertEqual(bookmarks_user1, [bookmark1, bookmark3, bookmark4])
+        self.assertEqual(bookmarks_user2_key2, [bookmark5])
+        self.assertEqual(bookmarks_instance1, [bookmark1, bookmark2, bookmark5])
+        self.assertEqual(bookmarks_instance2_user1, [bookmark3, bookmark4])
+        self.assertEqual(bookmarks_instance1_user2_key1, [bookmark2])
+        self.assertEqual(bookmarks_instance1_user1_key2, [])
+        
+    def test_remove_all_bookmarks_for_instance(self):
+        user1 = self.create_user('user_remove_all_1')
+        user2 = self.create_user('user_remove_all_2')
+        instance1 = self.create_instance('instance_remove_all_1')
+        instance2 = self.create_instance('instance_remove_all_2')
+        key1, key2 = 'key_remove_all_1', 'key_remove_all_2'
+
+        self.backend.add(user1, instance1, key1)
+        self.backend.add(user2, instance1, key1)
+        self.backend.add(user1, instance1, key2)
+        remaining = self.backend.add(user1, instance2, key1)
+
+        self.backend.remove_all_for(instance1)
+
+        bookmarks_instance1 = list(self.backend.filter_for(instance1))
+        bookmarks_instance2 = list(self.backend.filter_for(instance2))
+
+        self.assertEqual(bookmarks_instance1, [])
+        self.assertEqual(bookmarks_instance2, [remaining])
+
+    def test_bookmark_model(self):
+        user, instance, key = self.get_user_instance_key('model')
+        self.backend.add(user, instance, key)
+        bookmarks = list(self.backend.filter_by(user))
+        self.assertTrue(isinstance(bookmarks[0], self.backend.get_model()))
+    
+
+class DefaultBackendTestCase(unittest.TestCase, BaseBackendTest):
+    def setUp(self):
+        self.backend = backends.Backend()
