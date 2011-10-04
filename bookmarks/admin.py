629409845c9b973e864ed1209589ac96f90ea9ac
Francesco Banconi <frankban@gmail.com>
2011-10-04 17:42:14 +0200

    Fixed a reference to an unexistent key in the admin.

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from bookmarks import models
 
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ('content_object', 'key', 'user', 
-        'created_at', 'modified_at')
-    list_filter = ('content_type', 'created_at', 'modified_at')
-    ordering = ('-modified_at',)
+    list_display = ('content_object', 'key', 'user', 'created_at')
+    list_filter = ('content_type', 'created_at')
+    ordering = ('-created_at',)
     search_fields = ('user', 'key')
     readonly_fields = ('user',)
 
